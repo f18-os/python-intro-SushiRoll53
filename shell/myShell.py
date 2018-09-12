52bac858f15855a0db56b0c601fe594ea14fb1ae
SushiRoll53 <brfigueroa@miners.utep.edu>
2018-09-12 17:38:23 -0600

    Looks like the Pipe is workiing, but i don have how to test it V.2

--- shell/myShell.py
+++ shell/myShell.py
@@ -4,8 +4,12 @@ running = True
 while(running):
 	usr_command = input("Enter command: ")
 	args = usr_command.split(" ")
-	for i in args:
-		print(i)
+
+	if len(args) >= 3:
+		if args[1] == '|':
+			r, w = os.pipe()
+			os.set_inheritable(r, True) # To Read
+			os.set_inheritable(w, True) # To Write
 
 	pid = os.getpid()
 
@@ -18,32 +22,78 @@ while(running):
 	elif rc == 0:
 		os.write(1, ("##CHILD ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
 
-		if len(args) >= 3:
-			if args[2] == '<':
+		if len(args) < 3:
+			for dir in re.split(":", os.environ['PATH']): # try each directory in path
+				program = "%s/%s" % (dir, args[0])
+				try:
+					os.execve(program, args, os.environ) # try to exec program
+				except FileNotFoundError:             # ...expected
+					pass                              # ...fail quietly 
+
+			os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
+			sys.exit(1)
+
+		elif len(args) >= 3 and args[1] != '|':
+			if args[1] == '<':
 				temp = args[len(args)-3]
 				args[len(args)-3] = args[len(args)-1]
 				args[len(args)-1] = temp
-				args.remove("<")
-			try:
-				args.remove(">")
-			except ValueError:
-				pass
+			args.pop(1)
+
 			os.close(1)
 			sys.stdout = open(args[len(args)-1], "w")
 			fd = sys.stdout.fileno()
 			os.set_inheritable(fd, True)
 			os.write(2, ("##Child: opened fd=%d for writing\n" % fd).encode())
 
-		argsC = [args[0],args[1]]
-		for dir in re.split(":", os.environ['PATH']): # try each directory in path
-			program = "%s/%s" % (dir, args[0])
-			try:
-				os.execve(program, argsC, os.environ) # try to exec program
-			except FileNotFoundError:             # ...expected
-				pass                              # ...fail quietly 
+			argsC = [args[0],args[1]]
+			for dir in re.split(":", os.environ['PATH']): # try each directory in path
+				program = "%s/%s" % (dir, args[0])
+				try:
+					os.execve(program, argsC, os.environ) # try to exec program
+				except FileNotFoundError:             # ...expected
+					pass                              # ...fail quietly 
 
-		os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
-		sys.exit(1)
+			os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
+			sys.exit(1)
+
+		else:
+			args.pop(1)
+			rc1 = os.fork()
+
+			if rc1 < 0:
+				os.write(2, ("fork failed, returning %d\n" % rc1).encode())
+			elif rc1 == 0:
+				print("####GRANDCHILD")
+				os.close(1)
+				w = os.dup(w)
+
+				for dir in re.split(":", os.environ['PATH']): # try each directory in path
+					program = "%s/%s" % (dir, args[0])
+					try:
+						os.execve(program, args, os.environ) # try to exec program
+					except FileNotFoundError:             # ...expected
+						pass                              # ...fail quietly 
+
+				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
+				sys.exit(0)
+
+
+			else:
+				os.wait()
+				os.write(1, ("##CHILD AND PARENT ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+				os.close(0)
+				r = os.dup(r)
+
+				for dir in re.split(":", os.environ['PATH']): # try each directory in path
+					program = "%s/%s" % (dir, args[1])
+					try:
+						os.execve(program, args, os.environ) # try to exec program
+					except FileNotFoundError:             # ...expected
+						pass                              # ...fail quietly 
+
+				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[2]).encode())
+				sys.exit(0)
  
 
 	else:
