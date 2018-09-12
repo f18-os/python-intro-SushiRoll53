ec834f4693e0ac538ee8fbd096a30099410ae82e
SushiRoll53 <brfigueroa@miners.utep.edu>
2018-09-11 23:30:27 -0600

    Back to a working stage

--- shell/myShell.py
+++ shell/myShell.py
@@ -1,103 +1,49 @@
 import os, sys, time, re
 
-def getFiles(command,args):
-	pattern = re.compile(r'[A-Za-z0-9]+\.[A-Za-z]+')
-	matches = pattern.finditer(command)
-	for match in matches:
-		args.append(match[0])
-
-def getAction(command,args):
-	pattern = re.compile(r'[^A-Za-z0-9.\s]')
-	matches = pattern.finditer(command)
-	for match in matches:
-		args.append(match[0])
-
-
 running = True
 while(running):
 	usr_command = input("Enter command: ")
-	args = []
-
-	getFiles(usr_command,args) # gets the names of the files
-	getAction(usr_command,args) # gets what is going to be done
+	args = usr_command.split(" ")
+	for i in args:
+		print(i)
 
 	pid = os.getpid()
 
-	if len(args) == 3:
-		if args[2] == '|':
-			pipefds = os.pipe()
-			os.set_inheritable(pipefds[0], True)
-			os.set_inheritable(pipefds[1], True)
-			for i in pipefds:
-				print(i)
-
-		elif args[2] == '<':
-				temp = args[0]
-				args[0] = args[1]
-				args[1] = temp
-
 	os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
-	rc = os.fork() # First child
+	rc = os.fork()
 
 	if rc < 0:
 		os.write(2, ("fork failed, returning %d\n" % rc).encode())
 
 	elif rc == 0:
-		os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+		os.write(1, ("##CHILD ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
 
-		if args[2] == '|':
-			rc = os.fork() #Second child
-			
-			if rc != 0: # Grandchild
-				os.wait()
-				os.close(0)
-				pipefds[0] = open(0,"r")
-				#os.close(pipefds[0])
-				
-				for dir in re.split(":", os.environ['PATH']): # try each directory in path
-					program = "%s/%s" % (dir, args[1])
-					try:
-						os.execve(program, args, os.environ) # try to exec program
-					except FileNotFoundError:             # ...expected
-						pass                              # ...fail quietly 
-
-				os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-				#sys.exit(1)
-
-
-			else: # Child-Parent
-				
-				os.close(1)
-				pipefds[1] = open(1,"w")
-				#os.close(pipefds[1])
-
-				for dir in re.split(":", os.environ['PATH']): # try each directory in path
-					program = "%s/%s" % (dir, args[0])
-					try:
-						os.execve(program, args, os.environ) # try to exec program
-					except FileNotFoundError:             # ...expected
-						pass                              # ...fail quietly 
-
-				os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-				#sys.exit(1)
-
-		else:
+		if len(args) >= 3:
+			if args[2] == '<':
+				temp = args[len(args)-3]
+				args[len(args)-3] = args[len(args)-1]
+				args[len(args)-1] = temp
+				args.remove("<")
+			try:
+				args.remove(">")
+			except ValueError:
+				pass
 			os.close(1)
-			sys.stdout = open(args[1], "w")
+			sys.stdout = open(args[len(args)-1], "w")
 			fd = sys.stdout.fileno()
 			os.set_inheritable(fd, True)
-			os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+			os.write(2, ("##Child: opened fd=%d for writing\n" % fd).encode())
 
+		argsC = [args[0],args[1]]
+		for dir in re.split(":", os.environ['PATH']): # try each directory in path
+			program = "%s/%s" % (dir, args[0])
+			try:
+				os.execve(program, argsC, os.environ) # try to exec program
+			except FileNotFoundError:             # ...expected
+				pass                              # ...fail quietly 
 
-			for dir in re.split(":", os.environ['PATH']): # try each directory in path
-				program = "%s/%s" % (dir, args[0])
-				try:
-					os.execve(program, args, os.environ) # try to exec program
-				except FileNotFoundError:             # ...expected
-					pass                              # ...fail quietly 
-
-			os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-			sys.exit(1)
+		os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
+		sys.exit(1)
  
 
 	else:
