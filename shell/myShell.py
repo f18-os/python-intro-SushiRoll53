7ad7f57558e643dddb94a07aacd8a37654e83e2f
SushiRoll53 <brfigueroa@miners.utep.edu>
2018-09-13 15:15:30 -0600

    Pipe is working needs lots of cleaning

--- shell/myShell.py
+++ shell/myShell.py
@@ -59,44 +59,57 @@ while(running):
 
 		else:
 			args.pop(1)
-			rc1 = os.fork()
-
+			rc1 = os.fork() # Creates a Grandchild
 			if rc1 < 0:
 				os.write(2, ("fork failed, returning %d\n" % rc1).encode())
 			elif rc1 == 0:
-				print("####GRANDCHILD")
+				print("####GRANDCHILD - will write into the pipe")
 				os.close(1)
-				w = os.dup(w)
+				os.dup(w)
+				os.set_inheritable(1, True) 
+				for i in (r, w):
+					os.close(i)
+				sys.stdout = os.fdopen(1,'w')
+				fdw = sys.stdout.fileno()
+				os.write(2, ("FDW = %d\n" % fdw).encode())
 
 				for dir in re.split(":", os.environ['PATH']): # try each directory in path
 					program = "%s/%s" % (dir, args[0])
+					print ("grandchild (input to pipe) trying to run", args[0])
 					try:
 						os.execve(program, args, os.environ) # try to exec program
 					except FileNotFoundError:             # ...expected
 						pass                              # ...fail quietly 
 
 				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
-				sys.exit(0)
+				#sys.exit(0)
 
 
 			else:
+				print("#####CHILD - will read from the pipe")
+				os.close(w)
 				os.wait()
 				os.write(1, ("##CHILD AND PARENT ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
 				os.close(0)
-				r = os.dup(r)
+				os.dup(r)
+				os.set_inheritable(0, True) 
+				os.close(r)
+				sys.stdin = os.fdopen(0,'r')
 
 				for dir in re.split(":", os.environ['PATH']): # try each directory in path
+					print("child (who reads from pipe) trying to exec ", args[1])
 					program = "%s/%s" % (dir, args[1])
 					try:
 						os.execve(program, args, os.environ) # try to exec program
 					except FileNotFoundError:             # ...expected
 						pass                              # ...fail quietly 
 
-				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[2]).encode())
-				sys.exit(0)
+				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[1]).encode())
+				#sys.exit(0)
  
 
 	else:
+		os.close(w)
 		os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
 		childPidCode = os.wait()
 		os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
