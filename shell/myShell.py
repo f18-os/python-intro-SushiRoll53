1c1866d64c126768c8d4c51be6b1367d2d321cf4
SushiRoll53 <brfigueroa@miners.utep.edu>
2018-09-15 19:20:47 -0600

    Added test cases and improved code

--- shell/myShell.py
+++ shell/myShell.py
@@ -1,6 +1,11 @@
 import os, sys, time, re
+promp = "$ "
 while(True):
-	usr_command = input("$ ")
+	usr_command = input(promp)
+	check = usr_command.split("=")
+	if check[0] == "export PS1 " or check[0] == "export PS1":
+		promp = check[1].replace("\"","").replace(" ","")
+		continue
 	if usr_command.lower() == "exit":
 		break
 	args = usr_command.split(" ")
@@ -11,27 +16,23 @@ while(True):
 			os.set_inheritable(r, True) # To Read
 			os.set_inheritable(w, True) # To Write
 
-	pid = os.getpid()
+	pid = os.getpid() #Parent's ID
 
 	os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 	rc = os.fork()
 
-	if rc < 0:
-		os.write(2, ("fork failed, returning %d\n" % rc).encode())
-
-	elif rc == 0:
+	if rc == 0:
 		os.write(1, ("##CHILD ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
 
 		if len(args) < 3:
 			for dir in re.split(":", os.environ['PATH']): # try each directory in path
-				program = "%s/%s" % (dir, args[0])
+				program = "%s/%s" % (".", args[0])
 				try:
 					os.execve(program, args, os.environ) # try to exec program
 				except FileNotFoundError:             # ...expected
 					pass                              # ...fail quietly 
 
 			os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
-			sys.exit(1)
 
 		elif len(args) >= 3 and args[1] != '|':
 			if args[1] == '<':
@@ -48,46 +49,38 @@ while(True):
 
 			argsC = [args[0],args[1]]
 			for dir in re.split(":", os.environ['PATH']): # try each directory in path
-				program = "%s/%s" % (dir, args[0])
+				program = "%s/%s" % (".", args[0])
 				try:
 					os.execve(program, argsC, os.environ) # try to exec program
 				except FileNotFoundError:             # ...expected
 					pass                              # ...fail quietly 
 
 			os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
-			sys.exit(1)
 
 		else:
 			args.pop(1)
 			os.write(1, ("About to fork (pid=%d)\n" % os.getpid()).encode())
 			rc1 = os.fork() # Creates a Grandchild
-			if rc1 < 0:
-				os.write(2, ("fork failed, returning %d\n" % rc1).encode())
-			elif rc1 == 0:
+			if rc1 == 0:
 				print("####GRANDCHILD - will write into the pipe")
 				os.close(1)
 				os.dup(w)
 				os.set_inheritable(1, True) 
 				for i in (r, w):
 					os.close(i)
-				sys.stdout = os.fdopen(1,'w')
-				fdw = sys.stdout.fileno()
-				os.write(2, ("FDW = %d\n" % fdw).encode())
+				sys.stdout = os.fdopen(1,'w') 
 
 				for dir in re.split(":", os.environ['PATH']): # try each directory in path
-					program = "%s/%s" % (dir, args[0])
-					print ("grandchild (input to pipe) trying to run", args[0])
+					program = "%s/%s" % (".", args[0])
 					try:
 						os.execve(program, args, os.environ) # try to exec program
 					except FileNotFoundError:             # ...expected
 						pass                              # ...fail quietly 
 
 				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[0]).encode())
-				#sys.exit(0)
-
 
 			else:
-				print("#####CHILD - will read from the pipe")
+				print("####CHILD - will read from the pipe")
 				os.close(w)
 				os.wait()
 				os.write(1, ("##CHILD AND PARENT ID=%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
@@ -98,17 +91,13 @@ while(True):
 				sys.stdin = os.fdopen(0,'r')
 
 				for dir in re.split(":", os.environ['PATH']): # try each directory in path
-					print("child (who reads from pipe) trying to exec ", args[1])
-					program = "%s/%s" % (dir, args[1])
+					program = "%s/%s" % (".", args[1])
 					try:
 						os.execve(program, args, os.environ) # try to exec program
 					except FileNotFoundError:             # ...expected
 						pass                              # ...fail quietly 
 
 				os.write(2, ("##Child:    Error: Could not exec %s\n" % args[1]).encode())
-				#sys.exit(0)
- 
-
 	else:
 		os.close(w)
 		os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
---
+++ shell/test.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+import re
+
+print("2")
+
+print("3")
+
+print("20")
